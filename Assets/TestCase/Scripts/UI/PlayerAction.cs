0d50c1101d3d8345f219939e5cc069e4fbd464ae
Nimblebody <62288670+Nimblebody@users.noreply.github.com>
2023-05-25 11:57:36 +0900

    23.05.25
    1.getItemPanel fixed

--- Assets/TestCase/Scripts/UI/PlayerAction.cs
+++ Assets/TestCase/Scripts/UI/PlayerAction.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class PlayerAction : MonoBehaviour
 {
     [SerializeField] Transform _cam;
+    [SerializeField] GameObject _getItemPanel;
     public float _playerActionDistance;
-    bool active = false;
+    public bool active = false;
     RaycastHit hit;
     void Update(){
         active = Physics.Raycast(_cam.position,_cam.TransformDirection(Vector3.forward), out hit, _playerActionDistance);
-
+        if (active == true && hit.collider.CompareTag("Weapon"))
+        {
+            _getItemPanel.SetActive(true);
+        }
+        if(active == false)
+        {
+            _getItemPanel.SetActive(false);
+        }
         if(Input.GetKeyDown(KeyCode.F) && active == true){
             
         }
+
     }
 }
